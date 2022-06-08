c97aa2b5a28a85aa622cb59b10d76f1e51d38196
sekkun foo <sekkun.luke@gmail.com>
2022-06-08 13:54:13 +0800

    added preferences model and link it to chores and users

---
+++ db/migrate/20220608055319_create_preferences.rb
@@ -0,0 +1,11 @@
+class CreatePreferences < ActiveRecord::Migration[6.1]
+  def change
+    create_table :preferences do |t|
+      t.integer :rating
+      t.references :user, null: false, foreign_key: true
+      t.references :chores, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
