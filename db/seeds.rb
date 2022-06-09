0609ed1583cc72a106e24416dd2f2c87f3cb7c54
sekkun foo <sekkun.luke@gmail.com>
2022-06-09 15:35:33 +0800

    added min seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,43 @@
 puts "Cleaning database"
 # Call destroy method
 
-puts "Creating seeds..."
+puts "Creating users..."
 user = User.new({
   name: "Alex"
   email: "alex@gmail.com"
 })
+user.save
+puts "user created"
+
+puts "creating flats and link user to flat"
+flat = Flat.new({
+  name: "room8"
+  token: "abc123qeytc"
+})
+flat.save
+
+flat_user = FlatUser.new
+flat_user.user = user
+flat_user.flat = flat
+flat_user.save
+puts "created flat and user"
+
+
+puts "creating chores"
+sweep = Chores.new({
+  name: "Sweep da floor"
+  frequency: "Daily"
+  repetition: 1
+  duration: 30 # To convert 30 to 30mins
+})
+sweep.flat = flat
+sweep.save
+
+laundry = Chores.new({
+  name: "Do them laundry"
+  frequency: "week"
+  repetition: 1
+  duration: 120 # To convert 120 to 120mins
+})
+laundry.flat = flat
+laundry.save
