24a65809dd719747d2503434f6833e1d596749c8
sekkun foo <sekkun.luke@gmail.com>
2022-06-09 23:17:38 +0800

    added flats ctrl

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,13 +1,17 @@
 class FlatsController < ApplicationController
-  def show
 
+  def create
+  end
+
+  def add_flatmates
+  end
+
+  def show
   end
 
   def edit
-
   end
 
   def update
-
   end
 end
