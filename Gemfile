990992e5aefc82e3445e50728813038c007ad8ed
Victor Duverne <victor.duverne@gmail.com>
2022-06-10 12:54:23 +0800

    started lofi seed, getting lunch

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'sidekiq-failures', '~> 1.0'
 gem 'bootsnap', '>= 1.4.4', require: false
 
 gem 'devise'
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
 
 gem 'autoprefixer-rails', '10.2.5'
 gem 'font-awesome-sass', '~> 5.6.1'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,54 +1,114 @@
-# Seeds (minimum)
-# Pre-set chores list
-# 1 user
-# 1 flat
+chores_array = ["Sweep the floor", "Throw out the garbage", "Pay the rent", "Do the dishes", "Do the laundry"]
+frequencies_array = ["daily", "weekly", "monthly"]
+repetitions_array = [1, 2, 3]
+durations_array = [30, 60, 90, 120]
+seed_quotes = ['“Seeds never lose their potential, not even in dirt.”', '“A seed has a few friends, but a tree has many enemies.”', '“All memories carry the seeds of suffering.”', '“Show me your seed and I ll show you your harvest.”']
 
-puts "Cleaning database"
-# Call destroy method
-User.destroy_all
-Flat.destroy_all
-Chore.destroy_all
-FlatUser.destroy_all
+# DB wipeout
+puts "Cleaning database..."
+Users.destroy_all
+Flat_users.destroy_all
+Chores.destroy_all
+Flats.destroy_all
+Chore_lists.destroy_all
+Month_lists.destroy_all
+Preferences.destroy_all
+puts "Database cleaned 👍"
+puts "Starting to seed..."
 
+
+#  ----
+# User creation
 puts "Creating users..."
 user = User.new({
-  name: "Alex",
-  email: "alex@gmail.com",
+  name: "admin",
+  email: "admin@gmail.com",
   password: "123123"
 })
 user.save!
-puts "user created"
+3.times do
+  user = User.new({
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: "123123"
+  })
+  user.save!
+end
+puts "Created 4 users"
 
-puts "creating flats and link user to flat"
+
+# Flat creation (for now the creating one)
+puts "creating 1 flat and linking users to the flat"
 flat = Flat.new({
-  name: "room8",
+  name: "Room8",
   token: "abc123qeytc"
 })
 flat.save!
 
-flat_user = FlatUser.new
-flat_user.user = user
-flat_user.flat = flat
-flat_user.save!
+User.all.each do |user|
+  flat_user = FlatUser.new
+  flat_user.user = user
+  flat_user.flat = flat
+  flat_user.save!
+end
 puts "created flat and user"
 
 
-puts "creating chores"
-sweep = Chore.new({
-  name: "Sweep da floor",
-  frequency: "Daily",
-  repetition: 1,
-  duration: 30 # To convert 30 to 30mins
-})
-sweep.flat = flat
-sweep.save
+# Chore creation
+Flat.all.each do |flat|
+  rand(4..8) do
+    chore = Chore.new({
+      name: chores_array.sample,
+      frequency: frequencies_array.sample,
+      repetition: repetitions_array.sample,
+      duration: durations_array.sample
+    })
+    chore.flat = flat
+    chore.save!
+  end
+end
 
-laundry = Chore.new({
-  name: "Do them laundry",
-  frequency: "week",
-  repetition: 1,
-  duration: 120 # To convert 120 to 120mins
-})
-laundry.flat = flat
-laundry.save
-puts "Min seeds completed"
+# Creating a chore list
+Flat.all.each do |flat|
+  flat.chores.each do |chore|
+    chore_list = Chore_list.new
+    chore_list.chore = chore
+    chore_list.deadline = Date.today+rand(100)
+    chore_list.user = flat.users.sample
+  end
+end
+
+
+
+
+puts "Lowfi seeds completed 👍"
+puts "Now loading seed quote..."
+sleep 2
+puts seed_quotes.sample
+
+# Month_lists
+# Chore_lists
+# Preferences
+
+# create_table "preferences", force: :cascade do |t|
+#   t.integer "rating"
+#   t.bigint "user_id", null: false
+#   t.bigint "chores_id", null: false
+#   t.datetime "created_at", precision: 6, null: false
+#   t.datetime "updated_at", precision: 6, null: false
+#   t.index ["chores_id"], name: "index_preferences_on_chores_id"
+#   t.index ["user_id"], name: "index_preferences_on_user_id"
+# end
+
+# create_table "chore_lists", force: :cascade do |t|
+#   t.datetime "deadline"
+#   t.boolean "complete", default: false
+#   t.bigint "month_list_id", null: false
+#   t.bigint "user_id", null: false
+#   t.bigint "chores_id", null: false
+#   t.datetime "created_at", precision: 6, null: false
+#   t.datetime "updated_at", precision: 6, null: false
+#   t.index ["chores_id"], name: "index_chore_lists_on_chores_id"
+#   t.index ["month_list_id"], name: "index_chore_lists_on_month_list_id"
+#   t.index ["user_id"], name: "index_chore_lists_on_user_id"
+# end
