449831088107b79a2fb4e6b069ca2c9932341f6d
wanxin pua <wanxinpua@yahoo.com.sg>
2022-06-10 16:12:20 +0800

    merged master

--- app/javascript/controllers/setup_chores_controller.js
+++ app/javascript/controllers/setup_chores_controller.js
@@ -35,4 +35,17 @@ export default class extends Controller {
     const modal = document.getElementById("addChoreModal");
     modal.style.display = "none";
   }
+  send(event) {
+    event.preventDefault()
+
+    fetch(this.formTarget.action, {
+      method: "POST",
+      headers: { "Accept": "application/json", "X-CSRF-Token": csrfToken() },
+      body: new FormData(this.formTarget)
+    })
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data)
+      })
+  }
 }
