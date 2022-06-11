9af3f73a149f3b21e82621dbdd4b39f766b26851
sekkun foo <sekkun.luke@gmail.com>
2022-06-12 02:23:00 +0800

    added swiper

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,14 +3,45 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
 
-import "controllers"
-import "bootstrap"
+import "controllers";
+import "bootstrap";
+
+import Swiper from "swiper";
+
+// swiper / scss / bundle;
+import "swiper/swiper-bundle.css";
+// import "swiper/swiper-bundle.min.css";
+// import "swiper/swiper.min.css";
+
+// init Swiper:
+// const swiper = new Swiper(...);
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: ".swiper-scrollbar",
+  // },
+});
