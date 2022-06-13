8827d1e0bc153840787e6a963ec2c3a66e908e8b
sekkun foo <sekkun.luke@gmail.com>
2022-06-13 16:27:08 +0800

    added cloudinary

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   config.cache_classes = false
   # Do not eager load code on boot.
   config.eager_load = false
-  
+
   # Show full error reports.
   config.consider_all_requests_local = true
   # Open sent email on new chrome tab
@@ -43,7 +43,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :cloudinary
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -85,8 +85,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-
-  # cloudinary
-  config.active_storage.service = :cloudinary
 
 end
