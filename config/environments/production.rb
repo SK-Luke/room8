e98b7fbd5596ab06896a63c28cc21d1ec3f46b73
Kenny Wong <kken.w@hotmail.com>
2022-06-17 18:34:45 +0800

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,8 +6,8 @@ Rails.application.configure do
   # allow herokuapp to send mail
   config.action_mailer.default_url_options = { host: "the-room-8.herokuapp.com" }
   config.action_mailer.perform_deliveries = true
-  config.action_controller.asset_host = 'the-room-8.herokuapp.com'
-  config.action_mailer.asset_host = config.action_controller.asset_host
+  # config.action_controller.asset_host = 'the-room-8.herokuapp.com'
+  # config.action_mailer.asset_host = config.action_controller.asset_host
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
