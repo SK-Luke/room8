8a9dbc5e19b97cbda6d52656749fd4442b7887c5
sekkun foo <sekkun.luke@gmail.com>
2022-06-20 00:16:43 +0800

    replace confirmation for mark complete with modal

--- app/javascript/controllers/mark_complete_controller.js
+++ app/javascript/controllers/mark_complete_controller.js
@@ -5,6 +5,7 @@ export default class extends Controller {
   static targets = ["tick", "chore"];
   static values = {
     taskid: String,
+    task: String,
   };
 
   // connect() {
@@ -16,7 +17,17 @@ export default class extends Controller {
 
   mark(event) {
     event.preventDefault();
-    if (confirm(`Have you ${this.choreTarget.dataset.value}?`)) {
+    // let mark = false;
+    console.log(document.getElementById("mark-complete-done"));
+    console.log(this.choreTarget.innerText);
+    document.querySelector("#exampleModalLabel").innerText =
+      this.choreTarget.innerText;
+    // console.log(mark);
+    document.querySelector("#mark-complete").addEventListener("click", (e) => {
+      // mark = true;
+      // console.log(mark);
+      $("#exampleModal").modal("hide");
+
       fetch(`/chore_list/${this.tickTarget.dataset.value}`, {
         method: "PUT",
         headers: { Accept: "application/json", "X-CSRF-Token": csrfToken() },
@@ -27,6 +38,6 @@ export default class extends Controller {
           console.log(data);
           location.reload();
         });
-    }
+    });
   }
 }
